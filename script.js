97ba0598718f49cdf288001f6f621cf189dcc899
SonaVarshney <sonavarshney29@gmail.com>
2024-02-19 12:36:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const accessKey = "hdJBKiwV4zRF6LmWruG21AKkOjEx6aIkMOjE9kg93cE";
+
+const formElement = document.querySelector("form");
+const inputElement = document.querySelector("input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+  inputData = inputElement.value;
+  //craeting a dynamic url -> this will fetch all data from unspash website
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+  //JavaScript fetch() method is used to request data from a server. The request can be of any type of API that returns the data in JSON or XML. The fetch() method requires one parameter, the URL to request, and returns a promise.
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results; // this will give us all the results from the api
+
+  if (page == 1) {
+    searchResults.innerHTML = "";
+  }
+
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result"); //adding a class to the div
+    const image = document.createElement("img");
+    image.src = result.urls.small; //small is unspalsh ka thumbnail size
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+  });
+
+  page++;
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
+}
+
+//calling the function
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMore.addEventListener("click", () => {
+  searchImages();
+});
